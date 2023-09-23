4cd06fb3db5c760c8fc922b531c62a30edf3f3b1
g.sebastian27 <g.sebastian27@yahoo.com>
2023-09-23 09:40:09 +0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { AboutComponent } from './about/about.component';
+import { TechComponent } from './tech/tech.component';
+import { WorkComponent } from './work/work.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, MainComponent, AboutComponent, TechComponent, WorkComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
